3b31335486246eae2211496f861e3003e00bd6c5
marcmaniez <marc.maniez@gmail.com>
2018-12-02 23:33:10 -0500

    Read properties from ethernet frame.

---
+++ pcap/datagram.go
@@ -0,0 +1,6 @@
+package main
+
+// Datagram adds convenience methods over an array of bytes
+type Datagram struct {
+	Data []byte
+}
--- pcap/ethernet_frame.go
+++ pcap/ethernet_frame.go
@@ -4,3 +4,28 @@ package main
 type EthernetFrame struct {
 	Data []byte
 }
+
+// MacDestination returns a hex byte string of the mad destination address
+func (e *EthernetFrame) MacDestination() []byte {
+	return BytesToHex(e.Data[0:6])
+}
+
+// MacSource returns a hex byte string of the mad destination address
+func (e *EthernetFrame) MacSource() []byte {
+	return BytesToHex(e.Data[6:12])
+}
+
+// EtherType returns a hex byte string of the mad destination address - We are assuming no tag for the time being.
+func (e *EthernetFrame) EtherType() []byte {
+	return BytesToHex(e.Data[12:14])
+}
+
+// Payload returns a hex byte string of the mad destination address
+func (e *EthernetFrame) Payload() []byte {
+	return BytesToHex(e.Data[14:len(e.Data)])
+}
+
+// Datagram returns the datagram for the ethernet frame
+func (e *EthernetFrame) Datagram() Datagram {
+	return Datagram{Data: e.Payload()}
+}
