5db23f9252aa30e3765d39fb2297154a55425c7d
marcmaniez <marc.maniez@gmail.com>
2018-12-15 17:10:25 -0800

    Filter TCP segments for duplicates and server responses.

--- pcap/pcap_file_parser.go
+++ pcap/pcap_file_parser.go
@@ -17,13 +17,18 @@ func main() {
 	check(err)
 
 	pcap := NewPcap(file)
-	fmt.Printf("magic number: %s\n", pcap.LinkType())
 	tcpSegments := make([]TCPSegment, 0)
+	sequenceNumberSet := make(map[int]bool)
 	for _, ethernetFrame := range pcap.EthernetFrames() {
 		datagram := ethernetFrame.Datagram()
 		tcpSegment := datagram.Payload()
+		sequenceNumberAlreadyHandled := sequenceNumberSet[tcpSegment.SequenceNumber()]
+		if tcpSegment.SourcePort() == 80 && !sequenceNumberAlreadyHandled {
 		tcpSegments = append(tcpSegments, tcpSegment)
+			sequenceNumberSet[tcpSegment.SequenceNumber()] = true
 	}
+	}
+	fmt.Printf("Test segment source port: %d\n", tcpSegments[0].SequenceNumber())
 	sort.SliceStable(tcpSegments, func(i, j int) bool {
 		return tcpSegments[i].SequenceNumber() < tcpSegments[j].SequenceNumber()
 	})
